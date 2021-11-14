9ef20726b61e9adaa41a31329029f21333051cfb
Asel Alimbaeva <asel_19.86@mail.ru>
2021-11-14 06:50:59 +0300

    added codes

--- JS/script.js
+++ JS/script.js
@@ -147,3 +147,84 @@ const enabledScroll = () => {
         pageOverlay.textContent = '';
     });
 }
+
+{// содание карточек портфолио на основе данных из JSON
+
+    const COUNT_CARD = 3;
+    const portfolioList = document.querySelector('.portfolio__list');
+    const portfolioAdd = document.querySelector('.portfolio__add');
+
+
+    // получили данные из базы данных
+    const getData = () => fetch('db.json')
+        .then((response) => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                throw `Что то пошло не так попробуйто еще раз, ошибка ${response.status}`
+            }
+        })
+        .catch(error => console.error(error));
+
+    // создание хронилище для хронение и вывода данных
+
+    const createStor = async () => {
+        const data = await getData();
+        return {
+            data,
+            counter: 0,
+            count: COUNT_CARD,
+            get length() {
+                return this.data.length;
+            },
+            get cardData() {
+                const renderData = this.data.slice(this.counter, this.counter + this.count);
+                this.counter += renderData.length;
+                return renderData;
+            }
+        };
+    };
+
+    const renderCard = (data) => {
+        const cards = data.map(({ preview, year, type, client, image }) => {
+
+            const li = document.createElement('li');
+            li.classList.add('porfolio__item');
+
+            li.innerHTML = `
+                <article class="card" tabindex="0" role="button" aria-label="открыть макет"
+                    data-full-image="${image}">
+                    <picture class="card__picture">
+                        <source srcset="${preview}.avif" type="image/avif">
+                        <source srcset="${preview}.webp" type="image/webp">
+                        <img src="${preview}.jpg" alt="превью iphone" width="166" height="103">
+                    </picture>
+                    <p class="card__data">
+                        <span class="card__client">Клиент: ${client}</span>
+                        <time class="card__date" datetime="${year}">год: ${year}</time>
+                    </p>
+                    <h3 class="card__title">${type}</h3>
+                </article>
+            `;
+            return li;
+        })
+        portfolioList.append(...cards);
+    }
+
+    const initPortfolio = async () => {
+        const stor = await createStor();
+        renderCard(stor.cardData);
+        portfolioAdd.addEventListener('click', () => {
+            renderCard(stor.cardData);
+            // console.log(stor.cardData);
+            // console.log(stor.length, stor.counter);
+            if (stor.length === stor.counter) {
+                portfolioAdd.remove();
+            }
+        })
+    }
+
+    initPortfolio();
+
+
+}
